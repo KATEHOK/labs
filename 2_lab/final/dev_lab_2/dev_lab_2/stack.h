ac3a09821b85f7cfd5b98491ae31fd39eabbd300
katehok <a.timin2003@mail.ru>
2022-03-19 11:24:27 +0300

    Added docs for all existing functions

--- 2_lab/final/dev_lab_2/dev_lab_2/stack.h
+++ 2_lab/final/dev_lab_2/dev_lab_2/stack.h
@@ -6,8 +6,29 @@ typedef struct {
 	Release* pRelease;
 } Stack;
 
+/*
+* Инициализирует стек.
+* Параметры:
+* (Stack*) pStack - указатель  на стек;
+* (int) len - длина вектора (если используется стек-вектор).
+* Вернет:
+* 0 - успешно;
+* 1 - неуспешно.
+*/
 int stackInit(Stack*, int);
+
+
 int stackPush(Stack*, char*);
+
+
 int stackCopy(Stack*, char**);
+
+
 int stackPop(Stack*, char**);
+
+/*
+* Освобождает память, выделенную под стек.
+* Параметры:
+* (Stack*) pStack - указатель на стек.
+*/
 void stackFree(Stack*);
